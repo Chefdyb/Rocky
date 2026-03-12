954d4daea4fd44079b505d488391654a1957bcc4
Desmond Botchway <desmondbotchway52@gmail.com>
2026-03-12 16:55:20 +0000

    worked on using tag helpers in addition to data annotations to create fiels where customers can input their categories and create them

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebApplication1.Models
 {
@@ -8,7 +9,7 @@ namespace WebApplication1.Models
         public int Id { get; set; }
 
         public string Name { get; set; }
-
+        [DisplayName("Display Order")]
         public int DisplayOrder { get; set; }
     }
 }
