433c983218e1af420fdaaf8ee551f48bf4acc8f3
Desmond Botchway <desmondbotchway52@gmail.com>
2026-03-18 12:59:45 +0000

    added icons to the crud operations of this web app

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -24,7 +24,7 @@ namespace WebApplication1.Controllers
         // GET - CREATE
         public IActionResult Create()
         {
-             return View();
+            return View();
         }
 
         // POST - CREATE
@@ -40,5 +40,71 @@ namespace WebApplication1.Controllers
             }
             return View(obj);
         }
+    
+     
+        
+        // GET - EDIT
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0) 
+            {
+                return NotFound();
+            }
+
+            var obj = _db.Category.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        // POST - EDIT
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Category.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        // GET - DELETE
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var obj = _db.Category.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        // POST - DELETE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Category.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+                _db.Category.Remove(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            
+            return View(obj);
+        }
+
     }
 }
